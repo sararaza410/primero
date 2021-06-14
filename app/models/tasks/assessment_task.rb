e4a968e457be35828394b3644b87b09a3f780485
Sara Raza <sara.raza@xprolabs.com>
2021-06-14 20:11:00 +0500

    Comprehensive and initial assessment both tasks should come up

--- app/models/tasks/assessment_task.rb
+++ app/models/tasks/assessment_task.rb
@@ -9,13 +9,13 @@ module Tasks
     end
 
     def self.has_task?(record)
-      record.try(:assessment_due_date).present? &&
-      !record.try(:assessment_requested_on).present? &&
+      (record.try(:assessment_due_date).present? &&
+      !record.try(:assessment_requested_on).present?) ||
       record.try(:due_date_for_comprehensive_assessment).present?
     end
 
     def due_date
-      self.parent_case.assessment_due_date
+      self.parent_case.due_date_for_comprehensive_assessment || self.parent_case.assessment_due_date
     end
   end
 end
