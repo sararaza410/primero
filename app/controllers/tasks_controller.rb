37d87731615d899b11a661fa7c29551ca5db70ed
Sara Raza <sara.raza@xprolabs.com>
2021-06-25 20:52:54 +0500

    Tasks order

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,7 @@ class TasksController < ApplicationController
     cases = Child.by_owned_by.key(current_user.user_name)
             .select{|c| c.record_state && c.child_status == Child::STATUS_OPEN}
     @tasks = Task.from_case(cases)
+    @tasks = @tasks.reject{ |c| c.priority.nil? }.sort_by(&:priority) + @tasks.select{|c| c.priority.nil?}
     @tasks = @tasks.paginate
 
 
